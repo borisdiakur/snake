66530ae31606edb375d6006cafa3c8d2a490cc55
Boris Diakur <contact@borisdiakur.com>
2017-05-09 10:32:29 +0200

    Add grid template

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,7 +1,11 @@
 <template>
-  <div>
-    my grid
-  </div>
+  <ul>
+    <li class="row" v-for="row in rows">
+      <ul>
+        <li class="cell" v-for="cell in row" v-bind:class="{active: cell}"></li>
+      </ul>
+    </li>
+  </ul>
 </template>
 
 <script>
@@ -9,10 +13,31 @@ export default {
   name: 'grid',
   data () {
     return {
+      rows: [
+        [0, 0, 0],
+        [0, 1, 1],
+        [0, 0, 1]
+      ]
     }
   }
 }
 </script>
 
 <style scoped>
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+  .row {
+    height: 22px;
+  }
+  .cell {
+    display: inline-block;
+    width: 20px;
+    height: 20px;
+    border: solid 1px #ddd;
+  }
+  .active {
+    background-color: steelblue;
+  }
 </style>
