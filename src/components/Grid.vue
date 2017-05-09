fb714d6a2ad60b3d4df2bef8a83e859c2ee3b088
Boris Diakur <contact@borisdiakur.com>
2017-05-09 18:05:24 +0200

    Add moving the snake

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -9,14 +9,22 @@
 </template>
 
 <script>
+import Vue from 'vue'
 
+let direction = ''
 const width = 20
 const height = 20
+const interval = 100
 for (var rows = []; rows.push(new Array(width)) < height;);
+const snake = [
+  { x: 3, y: 5 }, // tail
+  { x: 4, y: 5 },
+  { x: 5, y: 5 }  // head
+]
 
 function draw (snake, rows) {
   snake.forEach(cell => {
-    rows[cell.y][cell.x] = cell
+    Vue.set(rows[cell.y], cell.x, true)
   })
 }
 
@@ -29,12 +37,48 @@ export default {
     }
   },
   created () {
-    this.snake = [
-      { x: 3, y: 5 }, // tail
-      { x: 4, y: 5 },
-      { x: 5, y: 5 }  // head
-    ]
-    draw(this.snake, this.rows)
+    // make it move
+    setInterval(() => {
+      if (!direction) return
+      const head = snake[snake.length - 1]
+      switch (direction) {
+        case 'l':
+          snake.push({x: head.x - 1, y: head.y})
+          break
+        case 'r':
+          snake.push({x: head.x + 1, y: head.y})
+          break
+        case 'u':
+          snake.push({x: head.x, y: head.y - 1})
+          break
+        case 'd':
+          snake.push({x: head.x, y: head.y + 1})
+          break
+      }
+      const tail = snake.shift()
+      draw(snake, this.rows)
+      setTimeout(() => { Vue.set(this.rows[tail.y], tail.x, undefined) }, interval / 2)
+    }, interval)
+    draw(snake, this.rows)
+
+    document.onkeydown = event => {
+      event = event || window.event
+      const charCode = (typeof event.which === 'number') ? event.which : event.keyCode
+      switch (charCode) {
+        case 38: // arrow up
+          direction = 'u'
+          break
+        case 40: // arrow down
+          direction = 'd'
+          break
+        case 37: // arrow left
+          direction = 'l'
+          break
+        case 39: // arrow right
+          direction = 'r'
+          break
+      }
+    }
   }
 }
 </script>
