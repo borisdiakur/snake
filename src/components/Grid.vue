763b6b84138477892f38e28d7e74e3a99fbc56c5
Boris Diakur <contact@borisdiakur.com>
2017-05-09 10:21:28 +0200

    Mod replaced hello component with grid component

---
+++ src/components/Grid.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    my grid
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'grid',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
