1fea15ee5b169a9a6213a7cb5e8c66e63887a341
Boris Diakur <contact@borisdiakur.com>
2017-05-10 11:50:07 +0200

    Add game over on collision

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -37,34 +37,58 @@ export default {
     }
   },
   created () {
+    // draw the initial snake
+    draw(snake, this.rows)
+
     // make it move
-    setInterval(() => {
+    const intervalId = setInterval(() => {
       if (!direction) return
       const head = snake[snake.length - 1]
+      let nextCell
       switch (direction) {
         case 'l':
-          snake.push({x: head.x - 1, y: head.y})
+          nextCell = {x: head.x - 1, y: head.y}
           break
         case 'r':
-          snake.push({x: head.x + 1, y: head.y})
+          nextCell = {x: head.x + 1, y: head.y}
           break
         case 'u':
-          snake.push({x: head.x, y: head.y - 1})
+          nextCell = {x: head.x, y: head.y - 1}
           break
         case 'd':
-          snake.push({x: head.x, y: head.y + 1})
+          nextCell = {x: head.x, y: head.y + 1}
           break
       }
+
+      // game over if the snake bites itself or moves out of grid
+      if (
+        snake.find(cell => cell.x === nextCell.x && cell.y === nextCell.y) ||
+        nextCell.x < 0 || nextCell.x >= width || nextCell.y < 0 || nextCell.y >= height
+      ) {
+        clearInterval(intervalId)
+        alert('Game over')
+        return
+      }
+
+      snake.push(nextCell)
       const tail = snake.shift()
       draw(snake, this.rows)
       setTimeout(() => { Vue.set(this.rows[tail.y], tail.x, undefined) }, interval / 2)
     }, interval)
-    draw(snake, this.rows)
 
+    // set direction on keyboard input
     document.onkeydown = event => {
       event = event || window.event
       const charCode = (typeof event.which === 'number') ? event.which : event.keyCode
       switch (charCode) {
+        case 37: // arrow left
+          if (direction === 'r' || !direction) return
+          direction = 'l'
+          break
+        case 39: // arrow right
+          if (direction === 'l') return
+          direction = 'r'
+          break
         case 38: // arrow up
           if (direction === 'd') return
           direction = 'u'
@@ -72,14 +96,6 @@ export default {
         case 40: // arrow down
           if (direction === 'u') return
           direction = 'd'
-          break
-        case 37: // arrow left
-          if (direction === 'r') return
-          direction = 'l'
-          break
-        case 39: // arrow right
-          if (direction === 'l') return
-          direction = 'r'
           break
       }
     }
