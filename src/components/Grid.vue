b61dee17adb58a4cc08cf12741902941038ec442
Boris Diakur <contact@borisdiakur.com>
2017-06-13 15:04:23 +0200

    refactoring

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -11,54 +11,56 @@
 <script>
 import Vue from 'vue'
 
-let currentDirection = ''
-let nextDirection = ''
-const width = 20
-const height = 20
-const interval = 100
-for (var rows = []; rows.push(new Array(width)) < height;);
-const snake = [
-  { x: 3, y: 5 }, // tail
-  { x: 4, y: 5 },
-  { x: 5, y: 5 }  // head
-]
-
-function draw (snake, rows) {
-  snake.forEach(cell => {
-    Vue.set(rows[cell.y], cell.x, 'snake')
-  })
-}
-
-function feed (snake, rows) {
-  let randomY, randomX
-  do {
-    randomY = Math.floor(Math.random() * height)
-    randomX = Math.floor(Math.random() * width)
-  } while (rows[randomY][randomX])
-  Vue.set(rows[randomY], randomX, 'mouse')
-}
-
 export default {
   name: 'grid',
   data () {
     return {
-      rows,
-      snake: []
+      rows: [],
+      snake: [],
+      currentDirection: '',
+      nextDirection: '',
+    }
+  },
+  methods: {
+    draw () {
+      this.snake.forEach(cell => {
+        Vue.set(this.rows[cell.y], cell.x, 'snake')
+      })
+    },
+    feed () {
+      if (!this.rows[0] || !this.rows[0].length) return
+      let randomY, randomX
+      do {
+        randomY = Math.floor(Math.random() * (this.rows[0]).length)
+        randomX = Math.floor(Math.random() * this.rows.length)
+      } while (this.rows[randomY][randomX])
+      Vue.set(this.rows[randomY], randomX, 'mouse')
     }
   },
   created () {
+    const width = 20
+    const height = 20
+    for (var rows = []; rows.push(new Array(width)) < height;);
+    this.rows = rows
+    this.snake = [
+      { x: 3, y: 5 }, // tail
+      { x: 4, y: 5 },
+      { x: 5, y: 5 }  // head
+    ]
+
     // draw the initial snake and feed it
-    draw(snake, this.rows)
-    feed(snake, this.rows)
+    this.draw()
+    this.feed()
 
     // make it move
+    const interval = 100
     const intervalId = setInterval(() => {
-      if (!nextDirection) return
-      currentDirection = nextDirection
+      if (!this.nextDirection) return
+      this.currentDirection = this.nextDirection
 
-      const head = snake[snake.length - 1]
+      const head = this.snake[this.snake.length - 1]
       let nextCell
-      switch (currentDirection) {
+      switch (this.currentDirection) {
         case 'l':
           nextCell = { x: head.x - 1, y: head.y }
           break
@@ -83,17 +85,17 @@ export default {
         return
       }
 
-      snake.push(nextCell)
-      const tail = snake.shift()
+      this.snake.push(nextCell)
+      const tail = this.snake.shift()
 
       // eat mouse
-      if (rows[nextCell.y][nextCell.x] === 'mouse') {
-        snake.unshift({ x: tail.x, y: tail.y })
-        draw(snake, this.rows)
-        feed(snake, this.rows)
+      if (this.rows[nextCell.y][nextCell.x] === 'mouse') {
+        this.snake.unshift({ x: tail.x, y: tail.y })
+        this.draw(this.snake, this.rows)
+        this.feed(this.snake, this.rows)
       } else {
         setTimeout(() => { Vue.set(this.rows[tail.y], tail.x, undefined) }, interval / 2)
-        draw(snake, this.rows)
+        this.draw(this.snake, this.rows)
       }
     }, interval)
 
@@ -103,20 +105,20 @@ export default {
       const charCode = (typeof event.which === 'number') ? event.which : event.keyCode
       switch (charCode) {
         case 37: // arrow left
-          if (currentDirection === 'r' || !currentDirection) return
-          nextDirection = 'l'
+          if (this.currentDirection === 'r' || !this.currentDirection) return
+          this.nextDirection = 'l'
           break
         case 39: // arrow right
-          if (currentDirection === 'l') return
-          nextDirection = 'r'
+          if (this.currentDirection === 'l') return
+          this.nextDirection = 'r'
           break
         case 38: // arrow up
-          if (currentDirection === 'd') return
-          nextDirection = 'u'
+          if (this.currentDirection === 'd') return
+          this.nextDirection = 'u'
           break
         case 40: // arrow down
-          if (currentDirection === 'u') return
-          nextDirection = 'd'
+          if (this.currentDirection === 'u') return
+          this.nextDirection = 'd'
           break
       }
     }
